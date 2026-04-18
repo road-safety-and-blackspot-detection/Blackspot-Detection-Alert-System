e04c7599b88507a23a5689841e8ea7018e97f79c
krn-zorel <kumar.karanq98@gmail.com>
2026-04-18 13:37:51 +0530

    Added ML engine and Expo mobile app structure

---
+++ mobile-app/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.
+        <Text>OH i got this ts is so easy</Text>
+      </Text>
+    </View>
+  );
+}
